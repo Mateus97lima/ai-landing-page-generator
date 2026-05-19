9d3f6cf5a7a9800ea296451a7ddba0fcecce840d
Mateus Lima <matheus2022y@gmail.com>
2026-05-19 20:07:54 -0300

    initial

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-10 flex-col items-center justify-center mt-11 gap-5" >
+      <p className="max-w-2xl text-[14px] font-bold text-center  text-roxo-500 border-2 border-gray-300 rounded-4xl p-1" >⚡ PromptPage AI — Transforme prompts em páginas prontas ⚡</p>
+      <h1>Crie sua Página <span>com um Prompt</span></h1>
+
+    </main>
+  );
+}
+  
