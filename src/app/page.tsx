0f64e9d53dda8dddca8578c9a8a2525b3357c126
Mateus Lima <matheus2022y@gmail.com>
2026-05-20 21:13:57 -0300

    Criando Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@
 export default function Home() {
   return (
     <main className="flex min-h-10 flex-col items-center justify-center mt-11 gap-5" >
-      <p className="max-w-2xl text-[14px] font-bold text-center  text-roxo-500 border-2 border-gray-300 rounded-4xl p-1" >⚡ PromptPage AI — Transforme prompts em páginas prontas ⚡</p>
-      <h1>Crie sua Página <span>com um Prompt</span></h1>
+      <p className="max-w-2xl text-[0.60em] lg:text-[0.78em] opacity-96 text-purple-400 font-bold text-center text-roxo-500 border-2 border-purple-600 bg-purple-[#a78bfa] rounded-4xl p-1" >⚡ PromptPage AI —  Gerador com inteligência artificial ⚡</p>
+      <h1 className="text-md lg:text-4xl lg:mt-10">Crie sua Página <span className="text-purple-400">com um Prompt</span></h1>
 
     </main>
   );
